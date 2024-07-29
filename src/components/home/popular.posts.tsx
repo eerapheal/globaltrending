f05af5b427f6a305beb038045695d2c0b1d9323f
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-30 03:03:14 +0530

    categories and popular post placeholder data

--- src/components/home/popular.posts.tsx
+++ src/components/home/popular.posts.tsx
@@ -1,8 +1,25 @@
+import { popularPosts } from '@/lib/placeholder.data'
+import Link from 'next/link'
 import React from 'react'
+import { Icons } from '../icons'
 
 const PopularPosts = () => {
   return (
-    <div>PopularPosts</div>
+    <div>
+      <ul className="overflow-auto">
+        {popularPosts.map((post) => (
+          <Link 
+            key={post}
+            href='/blog'
+            >
+            <li className="flex items-center gap-2 cursor-pointer py-2">
+              <Icons.arrowRight className='h-5 w-5' />
+              <p>{post.title}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
   )
 }
 
--- src/components/home/top.categories.tsx
+++ src/components/home/top.categories.tsx
@@ -1,9 +1,22 @@
+import { categories } from '@/lib/placeholder.data';
 import React from 'react'
+import { Button } from '../ui/button';
+import Link from 'next/link';
 
-const TopCatogories = () => {
+const TopCategories = () => {
   return (
-    <div>TopCatogories</div>
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
+      {categories.map((category) => (
+        <Button key={category}
+        variant={"secondary"}
+          className="hover:scale-110 transition-all"
+          asChild
+        >
+          <Link href={`/blog/${category}`}>{category}</Link>
+        </Button>
+      ))}
+    </div>
   )
 }
 
-export default TopCatogories
+export default TopCategories;
