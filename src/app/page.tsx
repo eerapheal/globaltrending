1bcd25fc3f8d95818741528314ab149b9df98924
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-29 06:33:48 +0530

    sample latest post rendered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import LatestPost from "@/components/home/latest.post";
+
 export default function Home() {
   return (
     <main className="text-blue-400">
       <div>
-        Home
+       <LatestPost />
       </div>
     </main>
   );
---
+++ src/components/home/latest.post.tsx
@@ -0,0 +1,42 @@
+import { formatDate, getBlogPosts } from '@/app/blog/utils'
+import Link from 'next/link';
+import React from 'react'
+
+const LatestPost = () => {
+  
+  let latestPost = getBlogPosts();
+
+  return (
+    <>
+      <h1>
+        Recently Published
+      </h1>
+      {
+        latestPost
+        .sort((a, b) => {
+if  (
+  new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+) {
+    return -1;
+}
+return 1;
+})
+.map((post) => (
+  <article key={post.slug} className="text-wrap max-w-md my-10">
+    <Link href={`/blog/${post.metadata.category}/${post.slug}`}>
+      <h3 className="font-bold py-2 leading-5 hover:text-blue-400">
+        {post.metadata.title}
+      </h3>
+    </Link>
+    <p className="leading-8 my-5">{post.metadata.summary}</p>
+    <p className="text-sm text-muted-foreground">
+      {formatDate(post.metadata.publishedAt)}
+    </p>
+  </article>
+))
+      }
+    </>
+  )
+}
+
+export default LatestPost
