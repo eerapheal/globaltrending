e5275fa0f70124c8ae5e4fca87167b79573c95e3
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-30 00:31:18 +0530

    wrap with theme provide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
+import Container from "@/components/Container";
 import LatestPost from "@/components/home/latest.post";
 import { MainNav } from "@/ui/header/mainNav";
 
 export default function Home() {
   return (
     <>
-    <header>
-      <MainNav />
-    </header>
-    <main className="">
-      <section>
-       <LatestPost />
-      </section>
-    </main>
+      <Container>
+        <header>
+          <MainNav />
+        </header>
+        <main className="">
+          <section>
+            <LatestPost />
+          </section>
+        </main>
+      </Container>
     </>
   );
 }
