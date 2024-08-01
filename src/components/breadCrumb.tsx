b07caae1f71ea35a7eaf1f589b3282b59ee92c25
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-08-01 05:44:29 +0530

    bread crumb route navigation

---
+++ src/components/breadCrumb.tsx
@@ -0,0 +1,34 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export function BreadcrumbWithCustomSeparator({
+  category,
+  slug,
+}: {
+  category: string;
+  slug: string;
+}) {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href={`/blog/${category}`}>{category}</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>{slug}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
