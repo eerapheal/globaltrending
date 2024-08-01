b69ed99c4a528e8ea0edd273884cee88d97a4c99
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-08-01 09:37:17 +0530

    contents rendered

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -2,8 +2,9 @@ import { BreadcrumbWithCustomSeparator } from '@/components/breadCrumb';
 import Container from '@/components/Container';
 import Header from '@/ui/header/header';
 import React from 'react';
-import { getBlogPosts } from '../../utils';
+import { formatDate, getBlogPosts } from '../../utils';
 import NotFound from '@/app/notFound';
+import CustomMDX from '@/components/mdx';
 
 const Page = ({ params }: { params: { category: string; slug: string } }) => {
   const post = getBlogPosts().find((post) => post.slug === params.slug);
@@ -20,8 +21,21 @@ const Page = ({ params }: { params: { category: string; slug: string } }) => {
             category={post.metadata.category}
             slug={post.slug}
           />
+         <h1 className="title font-semibold text-2xl tracking-tighter mt-4">
+            {post.metadata.title}
+          </h1>
+          <div className="flex justify-between items-center mt-2 mb-4 text-sm">
+            <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-2">
+              {formatDate(post.metadata.publishedAt)}
+            </p>
+          </div>
         </Container>
       </Header>
+      <Container>
+        <article className="prose">
+          <CustomMDX source={post.content} />
+        </article>
+      </Container>
     </>
   );
 };
