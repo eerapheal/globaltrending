da5ec9a9584a0d215a38fa303cc6e54ca50b53a9
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-08-03 23:59:48 +0530

    logout form

---
+++ src/components/profile/logoutForm.tsx
@@ -0,0 +1,11 @@
+import { logout } from "@/lib/profile/action";
+
+const LogoutForm = () => {
+  return (
+    <form action={logout}>
+      logout
+    </form>
+  )
+}
+
+export default LogoutForm;
