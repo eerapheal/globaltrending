8744827cd8851e5ab74393a889e2395231e7e891
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-08-06 09:22:36 +0530

    form data initialize

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { initialBlogFormData } from "@/utils/constants";
+import { BlogFormData } from "@/utils/type";
 import {
   Dispatch,
   ReactNode,
@@ -11,25 +13,30 @@ import {
 type ContextType = {
   loading: boolean;
   setLoading: Dispatch<SetStateAction<boolean>>;
-
+  formData: BlogFormData;
+  setFormData: Dispatch<SetStateAction<BlogFormData>>;
 };
 
 const initialState = {
   loading: false,
   setLoading: () => { },
-
+  formData: initialBlogFormData,
+  setFormData: () => {},
 };
 
 export const GlobalContext = createContext<ContextType>(initialState);
 
 export default function GlobalState({ children }: { children: ReactNode }) {
   const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState(initialBlogFormData);
 
   return (
     <GlobalContext.Provider
       value={{
         loading,
         setLoading,
+        formData,
+        setFormData,
       }}
     >
       {children}
