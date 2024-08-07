be411dc46400d3e46b591304fdfc328f0a8599ec
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-08-07 14:50:40 +0530

    CRUD file set up

---
+++ src/app/api/post/update-post/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function PUT(request: NextRequest) {
+  try {
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      success: false,
+      message:
+        "something went wrong ! please try again by refreshing the browser",
+    });
+  }
+}
